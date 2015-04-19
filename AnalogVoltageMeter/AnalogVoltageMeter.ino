53bfe0373bfba4df4f1484fbd3e5e4d4ce61cdc3
cory pisano <cory.pisano@gmail.com>
2015-04-19 09:55:41 -0400

    added AnalogVoltageMeter
    to read piezo voltage values at 4 Hz

---
+++ AnalogVoltageMeter/AnalogVoltageMeter.ino
@@ -0,0 +1,52 @@
+/*
+  Analog Input
+ Demonstrates analog input by reading an analog sensor on analog pin 0 and
+ turning on and off a light emitting diode(LED)  connected to digital pin 13. 
+ The amount of time the LED will be on and off depends on
+ the value obtained by analogRead(). 
+ 
+ The circuit:
+ * Potentiometer attached to analog input 0
+ * center pin of the potentiometer to the analog pin
+ * one side pin (either one) to ground
+ * the other side pin to +5V
+ * LED anode (long leg) attached to digital output 13
+ * LED cathode (short leg) attached to ground
+ 
+ * Note: because most Arduinos have a built-in LED attached 
+ to pin 13 on the board, the LED is optional.
+ 
+ 
+ Created by David Cuartielles
+ modified 30 Aug 2011
+ By Tom Igoe
+ 
+ This example code is in the public domain.
+ 
+ http://arduino.cc/en/Tutorial/AnalogInput
+ 
+ */
+
+int sensorPin = A1;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+  // Pin 13: Arduino has an LED connected on pin 13
+  // Pin 11: Teensy 2.0 has the LED on pin 11
+  // Pin  6: Teensy++ 2.0 has the LED on pin 6
+  // Pin 13: Teensy 3.0 has the LED on pin 13
+int sensorValue = 0;  // variable to store the value coming from the sensor
+double voltage = 0;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);  
+}
+
+void loop() {
+  delay(250);
+  voltage = sensorValue*5.0;
+  voltage = voltage/1023.0;
+  Serial.println(voltage);
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);    
+}
